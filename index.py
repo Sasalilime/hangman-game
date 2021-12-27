4ba53083eb9929a523d0f38ba87b0426fe078503
sasalilime <salimtison@gmail.com>
2021-12-27 23:36:03 +0100

    finish

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ secret = random.randint(0, len(words_list)-1)
 secret_word = words_list[secret]
 
 game = {
-    "secret_word": words_list,
+    "secret_word": secret_word,
     "guess_word": "_" * len(secret_word),
-    "life": len(secret_word)+5
+    "life": 10
 }
 
 print(f"{game['guess_word']} | vie(s): {game['life']}")
@@ -19,9 +19,10 @@ while True:
     letter = input('Entrez une lettre :')
     if letter in game["secret_word"] and letter not in game["guess_word"]:
         guess_word_list = list(game["guess_word"])
-        for index, current_letter in game['secret_word']:
+        for index, current_letter in enumerate(game['secret_word']):
             if current_letter == letter:
-
+                guess_word_list[index] = letter
+        game['guess_word'] = "".join(guess_word_list)
     elif letter not in game["secret_word"]:
         game["life"] -= 1
     print(f"{game['guess_word']} | vies : {game['life']}")
